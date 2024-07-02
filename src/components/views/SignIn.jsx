3dc910a619b064d45462cae646bff0f9fbc6a80f
Austin Comstock <austinjcomstock@gmail.com>
2024-07-02 12:55:45 -0600

    Updated background image

--- src/components/views/SignIn.jsx
+++ src/components/views/SignIn.jsx
@@ -12,7 +12,8 @@ const useStyles = makeStyles(() => ({
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    backgroundImage: "url(https://source.unsplash.com/featured/?abstract)",
+    backgroundImage:
+      "url(https://images.unsplash.com/photo-1604871000636-074fa5117945?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
     backgroundRepeat: "no-repeat",
     backgroundSize: "cover",
     backgroundPosition: "center",
